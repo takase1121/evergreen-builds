15b78849a2af85c0e43178bcdb14d03b9f0d763a
Takase <20792268+takase1121@users.noreply.github.com>
2023-08-12 23:08:43 +0800

    ci: better GYP parsing (#9)

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+REPOS="https://github.com/tree-sitter/tree-sitter-c
+https://github.com/tree-sitter/tree-sitter-cpp
+https://github.com/the-mikedavis/tree-sitter-diff
+https://github.com/tree-sitter/tree-sitter-go
+https://github.com/camdencheek/tree-sitter-go-mod
+https://github.com/MunifTanjim/tree-sitter-lua
+https://github.com/tree-sitter/tree-sitter-rust"
+
+clone_and_generate() {
+    printf '%s' "$REPOS" | xargs -d"\n" -n 1 -P 4 -t git clone --depth 1 --no-tags
+    for repo in $REPOS; do
+        set -- "$@" "${repo##*/}"
+    done
+   ./fake-gyp.py "$@"
+}
+
+clone_and_generate
