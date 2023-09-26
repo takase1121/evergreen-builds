ccb0153db25aa719568286d7043fdb93c749c30b
sammyette <torchedsammy@gmail.com>
2023-09-26 18:43:25 -0400

    chore: update builds with new supported languages

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 REPOS="https://github.com/tree-sitter/tree-sitter-c
 https://github.com/tree-sitter/tree-sitter-cpp
+https://github.com/CyberShadow/tree-sitter-d
 https://github.com/the-mikedavis/tree-sitter-diff
 https://github.com/tree-sitter/tree-sitter-go
 https://github.com/camdencheek/tree-sitter-go-mod
+https://github.com/tree-sitter/tree-sitter-javascript
+https://github.com/tree-sitter/tree-sitter-julia
 https://github.com/MunifTanjim/tree-sitter-lua
 https://github.com/tree-sitter/tree-sitter-rust"
 
