116b9c4102aa734f7ac03cd36cb20cd178a0fd4c
sammyette <torchedsammy@gmail.com>
2023-09-26 18:43:50 -0400

    chore: add zig

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ https://github.com/camdencheek/tree-sitter-go-mod
 https://github.com/tree-sitter/tree-sitter-javascript
 https://github.com/tree-sitter/tree-sitter-julia
 https://github.com/MunifTanjim/tree-sitter-lua
-https://github.com/tree-sitter/tree-sitter-rust"
+https://github.com/tree-sitter/tree-sitter-rust
+https://github.com/maxxnino/tree-sitter-zig"
 
 clone_and_generate() {
     printf '%s' "$REPOS" | xargs -d"\n" -n 1 -P 4 -t git clone --depth 1 --no-tags
